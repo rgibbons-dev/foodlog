dad1e861e245e1633a52249be7886434a2c3d5bf
Ryan Gibbons <rgibbonsdev@gmail.com>
2022-04-17 08:58:56 -0400

    initial commit

---
+++ foodlog/foodlog.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/urfave/cli/v2"
+	"github.com/xuri/excelize/v2"
+)
+
+func excel(date string, food string) {
+	f, err := excelize.OpenFile("C:\\Users\\ryan\\Documents\\other docs\\random_uni\\food-log.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		// close spreadsheet
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	rows, err := f.Rows("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//get the "current row"
+	row := strconv.Itoa(rows.TotalRows() + 1)
+	dateAxis := "A" + row
+	foodAxis := "B" + row
+	//write the entry into the file
+	if err := f.SetCellValue("Sheet1", dateAxis, date); err != nil {
+		fmt.Println(err)
+	}
+	if err := f.SetCellValue("Sheet1", foodAxis, food); err != nil {
+		fmt.Println(err)
+	}
+	//save
+	if err := f.Save(); err != nil {
+		fmt.Println(err)
+	}
+}
+func main() {
+	var date string
+	var food string
+
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "add an entry to the log",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "date",
+						Usage:       "date of the entry, in (m{m}/d{d}/yyyy)",
+						Destination: &date,
+						Required:    true,
+					},
+					&cli.StringFlag{
+						Name:        "food",
+						Usage:       "the item of food to be logged",
+						Destination: &food,
+						Required:    true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					excel(date, food)
+					return nil
+				},
+			},
+		},
+		Name:        "FoodLog",
+		Description: "keep track of what you eat and when",
+		Usage:       "use the 'add' subcommand to log your food entries",
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module foodlog/foodlog
+
+go 1.17
+
+require (
+	github.com/urfave/cli/v2 v2.3.0
+	github.com/xuri/excelize/v2 v2.5.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.3 // indirect
+	github.com/richardlehane/msoleps v1.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/xuri/efp v0.0.0-20210322160811-ab561f5b45e3 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
